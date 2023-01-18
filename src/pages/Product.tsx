04efc0bc06a7f5122971bbc45492524dad6f306c
Saad T <tariqs26@mcmaster.ca>
2023-01-18 16:52:25 -0500

    feature: update product page - import new api functions for fetching and deletion - on successful deletion, redirect to products page and rather than invalidating the cache, just remove the deleted product from the cache - update delete button callback

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,7 +1,6 @@
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import fetchProductById from 'api/fetchProductById';
-import removeProduct from 'api/removeProduct';
+import { fetchProduct, deleteProduct } from 'api/productsAPI';
 
 const Product = () => {
   const { id } = useParams();
@@ -10,19 +9,24 @@ const Product = () => {
 
   const { data, status, error } = useQuery({
     queryKey: ['products', id],
-    queryFn: () => fetchProductById(id as string),
-    staleTime: Infinity,
+    queryFn: () => fetchProduct(id as string),
+    staleTime: 60000,
     initialData: () => {
       return queryClient
-        .getQueryData<Array<{ _id: string }>>(['products'])
+        .getQueryData<Array<Product>>(['products'])
         ?.find((p) => p._id === id);
     },
   });
 
   const mutate = useMutation({
-    mutationFn: removeProduct,
+    mutationFn: deleteProduct,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['products'] });
+      queryClient.setQueryData(
+        ['products'],
+        (oldData: Array<{ _id: string }> | undefined) =>
+          oldData!.filter((p) => p._id !== id)
+      );
+      navigate(-1);
     },
   });
 
@@ -39,14 +43,7 @@ const Product = () => {
       <Link to={`/products/${data._id}/edit`} state={{ product: data }}>
         Edit
       </Link>
-      <button
-        onClick={() => {
-          mutate.mutate(data._id);
-          navigate(-1);
-        }}
-      >
-        Delete
-      </button>
+      <button onClick={() => mutate.mutate(data._id)}>Delete</button>
     </section>
   );
 };
